590e2eb1f36de4884e911be5510cf4c7814d3b57
T0NY24 <anperezcue@uide.edu.ec>
2024-06-25 00:59:00 -0500

    Fase_BetaV2

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def fondo():
+    return render_template('fondo.html')
+
+@app.route('/animacion')
+def animacion():
+    return render_template('animacion.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/CV')  # Define una función de vista para '/CV'
+def cv():
+    return render_template('CV.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
